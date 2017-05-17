b9f82b08e1ff9627630caee5c3147bdd0a8c6702
John Walter <john.walter@wassa.fr>
2017-05-17 19:40:41 +0200

    Installed a couple of new bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
             new AppBundle\AppBundle(),
         ];
